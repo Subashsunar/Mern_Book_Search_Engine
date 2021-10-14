f63bebca7ff3e88605f3e5e6d0b991782e0121b7
Subhash <subashsunar00@gmail.com>
2021-10-14 22:15:01 +0530

    Add typedef, resolver and mutation
    Signed-off-by: Subhash <subashsunar00@gmail.com>

---
+++ Develop/server/Schemas/resolvers.js
@@ -0,0 +1,39 @@
+const User = require('../models/User');
+const signToken = require('../utils/auth');
+module.exports = {
+    Query: {
+        me: async (parents, args, config, info) => {
+            const user = await User.findOne({ args });
+            return user;
+        },
+    },
+    Mutation : {
+        login: async (parents, { user }, config, info) => {
+            const foundUser = await User.findOne({ $or: [{ username: user.username }, { email: user.email }] });
+            const correctPw = await foundUser.isCorrectPassword(body.password);
+            const token = signToken(foundUser);
+            return { foundUser, token };
+        },
+        addUser: async (parents, { user }, config, info) => {
+            const createdUser = await User.create(user);
+            const token = signToken(createdUser);
+            return { createdUser, token };
+        },
+        saveBook: async (parents, { book }, config, info) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $addToSet: { savedBooks: book } },
+                { new: true, runValidators: true }
+              );
+            return updatedUser;
+        },
+        removeBook: async (parents, { bookId, _id }, config, info) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: _id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+              );
+            return updatedUser;
+        }
+    }
+}
---
+++ Develop/server/Schemas/typeDefs.js
@@ -0,0 +1,54 @@
+const { gql } = require('apollo-server-express');
+
+module.exports = gql`
+
+type Query {
+    me(_id: ID
+        username: String
+        email: String
+        ): User
+}
+
+type Mutation {
+    login(user: UserInput): Auth
+    addUser(user: UserInput): Auth
+    saveBook(book: BookInput): User
+    removeBook(bookId: ID!, _id: ID!): User
+}
+
+input UserInput {
+    username: String
+    email: String
+    password: String!
+}
+
+input BookInput {
+    bookId: ID!
+    authors: [String]!
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+}
+type Book {
+    bookId: ID!
+    authors: [String]!
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Auth {
+    token: String!
+    user: User
+}`;
